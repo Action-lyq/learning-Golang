eee70ee7761afdde4adc89b755b70d5bddd61d02
liuyongqiang <Action123>
2023-01-12 16:32:29 +0800

    练习函数不定长参数，命名返回值

--- chapter02/017.multiple/main.go
+++ chapter02/017.multiple/main.go
@@ -6,12 +6,17 @@ func main() {
 	//bmis := []float64{1.2, 3.3, 4.5}
 	avgBmi := calculateAvg(1, 2, 3, 4, 45, 6, 6, 7)
 	fmt.Println(avgBmi)
+	getScoresOfStudent()
 }
 
-func calculateAvg(bmis ...float64) float64 {
+func calculateAvg(bmis ...float64) (avgBmi float64) {
 	var total float64 = 0
 	for _, item := range bmis {
 		total += item
 	}
 	return total / float64(len(bmis))
+}
+
+func getScoresOfStudent() (chinese int, english int, math int, nature int) {
+	return 0, 0, 0, 0
 }
