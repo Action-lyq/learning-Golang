340b65b72ac710664ed6734ad4f49ae4d79fbff7
Action_lyq <792250476@qq.com>
2023-01-12 10:49:48 +0800

    Delete chapter01 directory

--- chapter01/02.const/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	const pi = 3.1415926
-	fmt.Println(pi)
-	//pi = 3.14
-}
--- chapter01/03.strfmt/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("字符串")
-	fmt.Printf("我叫%s", "ldd")
-}
